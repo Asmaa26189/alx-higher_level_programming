007c25f99e4db07c9c49c43b3988d28848544be9
Asmaa26189 <engasmaahesham@gmail.com>
2023-11-29 02:53:16 +0200

    29-11-2023

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,6 +3,6 @@ for x in range(0, 100):
     if x == 99:
         print("{}".format(x), end="")
     elif x < 10:
-        print("0{}, ".format(x), end="")
-    else:
+        x = '0'+str(x)
+    if x != 99:
         print("{}, ".format(x), end="")
