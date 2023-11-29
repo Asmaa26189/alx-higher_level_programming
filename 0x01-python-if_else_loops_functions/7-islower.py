01f4c81e4260600759b56ca137cdddacf829d846
Asmaa26189 <engasmaahesham@gmail.com>
2023-11-29 03:03:42 +0200

    29-11-2023

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-	if c == c.lower() and not c.isnumeric():
-		return True
-	else:
-		return False
+    if c == c.lower() and c.isalpha():
+        return True
+    else:
+        return False
