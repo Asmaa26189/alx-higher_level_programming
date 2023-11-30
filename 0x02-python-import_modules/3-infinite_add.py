536458948a4f07e33766de35f54da155961851cc
Asmaa26189 <engasmaahesham@gmail.com>
2023-11-30 17:53:19 +0200

    30-11-2023

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,7 +4,6 @@ if __name__ == "__main__":
     import sys
 
     total = 0
-    if(len(sys.argv) < 0):
-        for i in sys.argv:
-            total += int(i)
-        print("{}".format(total))
+    for i in sys.argv:
+        total += int(i)
+    print("{}".format(total))
