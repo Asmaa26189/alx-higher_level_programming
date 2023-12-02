119142bb00903f29bdcc975ceaa2634e0d867751
Asmaa26189 <engasmaahesham@gmail.com>
2023-12-02 16:00:57 +0200

    2-12-2023

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,4 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    # new_string = ""
-    # for x in my_string:
-    #     if x !='c' and x != 'C':
-    #         new_string += x
     new_string = [x for x in my_string if x !='c' and x != 'C']
     return ("".join(new_string))
