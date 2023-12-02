d09a1abf8c925a8865d7b9bd7d00298b5c989884
Asmaa26189 <engasmaahesham@gmail.com>
2023-12-02 15:24:33 +0200

    2-12-2023

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,7 +4,8 @@ def new_in_list(my_list, idx, element):
     if(idx < 0 or idx >=  len(my_list)):
         return my_list
     else:
-        for x in my_list:
-            newList.append(x)
+        # for x in my_list:
+        #     newList.append(x)
+        newList = [x for x in my_list]
         newList[idx] = element
         return newList
