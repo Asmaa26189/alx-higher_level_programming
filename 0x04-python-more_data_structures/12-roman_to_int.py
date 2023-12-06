da49f7e6189729eed6876c644530ea410ecc25b3
Asmaa26189 <engasmaahesham@gmail.com>
2023-12-06 21:50:17 +0200

    6-12-2023

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,14 +1,5 @@
 #!/usr/bin/python3
-# 12-roman_to_int.py
-# Brennan D Baraban <375@holbertonschool.com>
-
-
 def roman_to_int(roman_string):
-    """Converts a roman numeral to an integer."""
-    if (not isinstance(roman_string, str) or
-            roman_string is None):
-        return (0)
-
     roman_dict = {
             "I": 1,
             "V": 5,
@@ -18,16 +9,20 @@ def roman_to_int(roman_string):
             "D": 500,
             "M": 1000
     }
-    num = 0
+    intNum = 0
+    
+    if (not isinstance(roman_string, str) or
+        roman_string is None):
+        return (0)
 
-    for i in range(len(roman_string)):
-        if roman_dict.get(roman_string[i], 0) == 0:
+    for x in range(len(roman_string)):
+        if roman_dict.get(roman_string[x], 0) == 0:
             return (0)
 
-        if (i != (len(roman_string) - 1) and
-                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
-                num += roman_dict[roman_string[i]] * -1
+        if ((len(roman_string) - 1) != x  and
+                roman_dict[roman_string[x]] < roman_dict[roman_string[x + 1]]):
+                intNum += roman_dict[roman_string[x]] * -1
 
         else:
-            num += roman_dict[roman_string[i]]
-    return (num)
+            intNum += roman_dict[roman_string[x]]
+    return (intNum)
