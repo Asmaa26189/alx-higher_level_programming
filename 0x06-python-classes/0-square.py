a92c94afe0b9a63ac49ac46d4fa66a96b48d11e2
Asmaa26189 <engasmaahesham@gmail.com>
2023-12-19 11:43:06 +0200

    13-12-2023

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
+"""Class Square"""
 
 class Square:
-    
+    """square"""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 class Square:
+    """Class square"""
+
     def __init__(self, size):
+        """Initialize Square instance
+
+        Args:
+            size (int): size
+        """
         self.__size = size
