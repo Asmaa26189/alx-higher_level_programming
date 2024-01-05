086f87cf4aa4b96015ab511ff185bce078e396a2
Asmaa26189 <engasmaahesham@gmail.com>
2024-01-05 02:46:17 +0200

    5-1-2024

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -51,7 +51,6 @@ class Rectangle:
             return (0)
         return ((self.__width * 2) + (self.__height * 2))
 
-
     def __str__(self):
         """Return str"""
         if self.__width == 0 or self.__height == 0:
