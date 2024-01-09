c9cd6d6a4fe71c1f28245f7b1a93408b73242437
Asmaa26189 <engasmaahesham@gmail.com>
2024-01-09 18:15:19 +0200

    9-1-2024

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -6,11 +6,14 @@ class Student:
     """Student"""
 
     def __init__(self, first_name, last_name, age):
-        """Init"""
+        """__init__"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """to_json"""
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
