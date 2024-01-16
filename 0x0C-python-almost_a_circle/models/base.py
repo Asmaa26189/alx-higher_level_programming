f588c10845c7dc50e26f126864292fd0cea8f122
Asmaa26189 <engasmaahesham@gmail.com>
2024-01-16 12:14:18 +0200

    16-1-2024

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -11,7 +11,11 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """__init__"""
+        """__init__
+
+            Args:
+            id (int): id
+        """
         if id is not None:
             self.id = id
         else:
@@ -20,14 +24,22 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """to_json_string"""
+        """to_json_string
+
+            Args:
+            list_dictionaries (list): list
+        """
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """save_to_file"""
+        """save_to_file
+
+           Args:
+           list_objs (list): list
+        """
         filename = cls.__name__ + ".json"
         with open(filename, "w") as jsonfile:
             if list_objs is None:
@@ -38,14 +50,22 @@ class Base:
 
     @staticmethod
     def from_json_string(json_string):
-        """from_json_string"""
+        """from_json_string
+
+           Args:
+           json_string (str): str
+        """
         if json_string is None or json_string == "[]":
             return []
         return json.loads(json_string)
 
     @classmethod
     def create(cls, **dictionary):
-        """create"""
+        """create
+
+           Args:
+           dictionary (dictionary): dictionary
+        """
         if dictionary and dictionary != {}:
             if cls.__name__ == "Rectangle":
                 new = cls(1, 1)
@@ -56,7 +76,11 @@ class Base:
 
     @classmethod
     def load_from_file(cls):
-        """load_from_file"""
+        """load_from_file
+
+           Args:
+           dictionary (dictionary): dictionary
+        """
         filename = str(cls.__name__) + ".json"
         try:
             with open(filename, "r") as jsonfile:
@@ -67,7 +91,11 @@ class Base:
 
     @classmethod
     def save_to_file_csv(cls, list_objs):
-        """save_to_file_csv"""
+        """save_to_file_csv
+
+           Args:
+           list_objs (list): list
+        """
         filename = cls.__name__ + ".csv"
         with open(filename, "w", newline="") as csvfile:
             if list_objs is None or list_objs == []:
@@ -83,7 +111,11 @@ class Base:
 
     @classmethod
     def load_from_file_csv(cls):
-        """load_from_file_csv"""
+        """load_from_file_csv
+
+           Args:
+           cls (class): class
+        """
         filename = cls.__name__ + ".csv"
         try:
             with open(filename, "r", newline="") as csvfile:
@@ -100,7 +132,12 @@ class Base:
 
     @staticmethod
     def draw(list_rectangles, list_squares):
-        """draw"""
+        """draw
+
+           Args:
+           list_rectangles (list): list
+           list_squares (list): list
+        """
         turt = turtle.Turtle()
         turt.screen.bgcolor("#b7312c")
         turt.pensize(3)
