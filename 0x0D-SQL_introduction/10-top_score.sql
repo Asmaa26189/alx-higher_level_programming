2119fc1e343731d7e6634bc6f51ef1a80e04e6f5
Asmaa26189 <engasmaahesham@gmail.com>
2024-02-14 21:16:31 +0200

    1-2-2024

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- The script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server
 -- Records should be ordered by score (top first)
-SELECT (name, score) FROM second_table WHERE score >= 10 ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE ORDER BY score DESC;
