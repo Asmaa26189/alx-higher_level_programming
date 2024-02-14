bb3f9f5ceedfebea7a1008632414a1d7c8a4bd5b
Asmaa26189 <engasmaahesham@gmail.com>
2024-02-14 23:23:06 +0200

    1-2-2024

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,9 +1,9 @@
 -- The script that creates the MySQL server user user_0d_1
 CREATE USER
-    IF NOT EXISTS user_0d_1@localhost
+    IF NOT EXISTS user_0d_1
     IDENTIFIED BY user_0d_1_pwd;
 GRANT ALL PRIVILEGES
    ON *.*
-   TO user_0d_1@localhost
+   TO user_0d_1
    IDENTIFIED BY user_0d_1_pwd;
 FLUSH PRIVILEGES;
