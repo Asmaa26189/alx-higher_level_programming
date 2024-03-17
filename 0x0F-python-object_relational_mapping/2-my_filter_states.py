3245f3ef3b1d486b9a14b1989a42d24da7502cb7
Asmaa26189 <engasmaahesham@gmail.com>
2024-03-17 15:14:53 +0200

    17-3-2024

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-# Displays all values in the states table of the database hbtn_0e_0_usa
-# whose name matches that supplied as argument.
-# Usage: ./2-my_filter_states.py <mysql username> \
-#                                <mysql password> \
-#                                <database name> \
-#                                <state name searched>
+"""
+script
+"""
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
+    """
+    1-filter_states
+    """
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3])
     c = conn.cursor()
