e22705c34bb8997ffc625a4bcc7526f387e2616f
Asmaa26189 <engasmaahesham@gmail.com>
2024-03-17 14:24:06 +0200

    17-3-2024

--- 0x0F-python-object_relational_mapping/0-select_states.sql
+++ 0x0F-python-object_relational_mapping/0-select_states.sql
@@ -7,5 +7,5 @@ CREATE TABLE IF NOT EXISTS states (
   name VARCHAR(256) NOT NULL,
   PRIMARY KEY (id)
 );
-DELETE FROM states
+DELETE FROM states;
 INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
