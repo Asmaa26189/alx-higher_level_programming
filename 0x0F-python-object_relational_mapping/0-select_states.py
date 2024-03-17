2b2d456d8f9c9b407e03b9f1951120f59ac13466
Asmaa26189 <engasmaahesham@gmail.com>
2024-03-17 15:12:49 +0200

    17-3-2024

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This script lists all
+script
 """
 
 import MySQLdb
@@ -8,8 +8,7 @@ from sys import argv
 
 if __name__ == '__main__':
     """
-    Access to the database and get the states
-    from the database.
+    0-select_states
     """
     db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
                          passwd=argv[2], db=argv[3], charset="utf8")
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """
-This script lists all
+script
 """
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
     """
-    Access to the database and get the states
-    from the database.
+    1-filter_states
     """
     db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
                          passwd=argv[2], db=argv[3], charset="utf8")
