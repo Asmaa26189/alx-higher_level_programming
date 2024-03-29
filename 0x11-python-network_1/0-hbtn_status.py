44fb030610d10ddd10a082814349047f6e3d059a
Asmaa26189 <engasmaahesham@gmail.com>
2024-03-29 02:59:51 +0200

    23-3-2024

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-"""script """
+"""This script fetches data from
+    https://alx-intranet.hbtn.io/status
+"""
+
+
 import urllib.request
 
 with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
