772055a4c4e5aba9fbaffd7c01a3cf9cd4b95d83
Asmaa26189 <engasmaahesham@gmail.com>
2024-03-29 02:56:46 +0200

    23-3-2024

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""script"""
+
+import sys
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as resp:
+        print(dict(resp.headers).get("X-Request-Id"))
